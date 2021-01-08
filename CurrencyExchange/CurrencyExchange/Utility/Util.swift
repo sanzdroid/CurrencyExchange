5fbb9552c7f000dc724b97a4d9d8d8f11b39ddf1
sanzdroid <stha.it78@gmail.com>
2021-01-08 14:29:47 +1100

    fixed currency indicator and textfield delegate

---
+++ CurrencyExchange/CurrencyExchange/Utility/Util.swift
@@ -0,0 +1,53 @@
+//
+//  Util.swift
+//  CurrencyExchange
+//
+//  Created by sanzrush on 8/1/21.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func addDashedBorder(){
+        backgroundColor = .clear
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.name = "DashedTopLine"
+        shapeLayer.bounds = bounds
+        shapeLayer.position = CGPoint(x: frame.width / 2, y: frame.height*1.5)
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.black.cgColor
+        shapeLayer.lineWidth = 2.0
+        shapeLayer.lineDashPattern = [6, 4]
+        
+        let path = CGMutablePath()
+        path.move(to: CGPoint.zero)
+        path.addLine(to: CGPoint(x: frame.width, y: 0))
+        shapeLayer.path = path
+        
+        layer.addSublayer(shapeLayer)
+    }
+    func checkEmpty() -> Bool{
+        if self.text == ""{
+            return true
+        }
+        return false
+    }
+}
+
+func showAlert(title: String, message: String, viewController: UIViewController){
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+
+    viewController.present(alert, animated: true)
+}
+
+func dropShadow(view: UIView, color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
+    view.layer.masksToBounds = false
+    view.layer.shadowColor = color.cgColor
+    view.layer.shadowOpacity = opacity
+    view.layer.shadowOffset = offSet
+    view.layer.shadowRadius = radius
+}
--- CurrencyExchange/CurrencyExchange/ViewController.swift
+++ CurrencyExchange/CurrencyExchange/ViewController.swift
@@ -13,40 +13,45 @@ struct Forex: Decodable {
     let base, date: String
     let rates: [String: Double]
 }
-let url = "http://data.fixer.io/api/latest?access_key=94044bdcc5e504b1e71fd404dbff85f2"
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
+let url = "http://data.fixer.io/api/latest?access_key=94044bdcc5e504b1e71fd404dbff85f2&symbols=CAD,AUD,GBP,JPY,USD"
+
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UITextFieldDelegate {
+    var selectedIndex = 0
     @IBOutlet var lblConvertedCurrency: UILabel!
     @IBOutlet var txtAmount: UITextField!
     var keyArray = NSMutableArray()
     var valueArray = NSMutableArray()
-    var selectedIndex = 0
     
     @IBOutlet var curCollectionView: UICollectionView!
     
     override func viewWillLayoutSubviews() {
-        self.dropShadow(view:self.curCollectionView ,color: .green, opacity: 1, offSet: CGSize(width: -1, height: 1), radius: 2, scale: true)
+        dropShadow(view:self.curCollectionView ,color: .systemGreen, opacity: 1, offSet: CGSize(width: -1, height: 1), radius: 2, scale: true)
         self.txtAmount.addDashedBorder()
+        self.lblConvertedCurrency.accessibilityIdentifier = "curLabel"
+        self.txtAmount.accessibilityIdentifier = "curText"
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.txtAmount.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         self.getData()
+        
     }
     
     func getForexData(completion: @escaping (Result<Forex, Error>) -> ()){
-        let urlString = "\(url)&symbols=CAD,AUD,GBP,JPY,USD"
-        print(urlString)
-        guard let mainUrl = URL(string: urlString) else {return}
+        guard let mainUrl = URL(string: url) else {return}
         
         URLSession.shared.dataTask(with: mainUrl) { (data, response,error) in
             if let err = error{
                 completion(.failure(err))
                 return
             }
-            
             do{
                 let forex = try JSONDecoder().decode(Forex.self, from: data!)
                 completion(.success(forex))
@@ -67,14 +72,18 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "curCell", for: indexPath) as! curCell
         cell.lblCurrency.text = self.keyArray[indexPath.row] as? String
+        if indexPath.row == self.selectedIndex{
+            cell.lblCurrency.alpha = 1.0
+        }else{
+            cell.lblCurrency.alpha = 0.5
+        }
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.selectedIndex = indexPath.row
         self.curCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-        let amount = Float(self.txtAmount.text ?? "0") ?? 0
-        let unit: Float = self.valueArray[indexPath.row] as! Float
-        let convertedAmt = amount * unit
-        self.lblConvertedCurrency.text = "\(convertedAmt)"
+        self.lblConvertedCurrency.text = self.calculateAmount(text: self.txtAmount.text ?? "", index: indexPath.row)
+        self.curCollectionView.reloadData()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -83,19 +92,29 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        let horizontalInset = self.curCollectionView.frame.size.width/2 - 40
+        let horizontalInset = self.curCollectionView.frame.size.width/2-40
         
         return UIEdgeInsets(top: 0,left: horizontalInset,bottom: 0,right: horizontalInset)
     }
+    //MARK: UITextfield Delegate
+//        func textFieldDidBeginEditing(_ textField: UITextField) {
+//            self.lblConvertedCurrency.text = self.calculateAmount(textfield: self.txtAmount, index: self.selectedIndex)
+//        }
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            var fullString = "\(textField.text ?? "")\(string)"
+            
+            if let char = string.cString(using: String.Encoding.utf8) {
+                    let isBackSpace = strcmp(char, "\\b")
+                    if (isBackSpace == -92) {
+                        fullString = ""
+                        self.txtAmount.text = ""
+                    }
+                }
+            
+            self.lblConvertedCurrency.text = self.calculateAmount(text: fullString, index: self.selectedIndex)
+            return true
+        }
     
-    //MARK: Custom Methods
-    func dropShadow(view: UIView, color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
-        view.layer.masksToBounds = false
-        view.layer.shadowColor = color.cgColor
-        view.layer.shadowOpacity = opacity
-        view.layer.shadowOffset = offSet
-        view.layer.shadowRadius = radius
-    }
     
     //MARK: Get Data
     func getData(){
@@ -115,6 +134,36 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             case .failure(let error):
                 print("failed to get data", error)
             }
+        }
+    }
+    
+    func calculateAmount(text: String, index: Int) -> String{
+        let amount = Float(text) ?? 0
+        let unit: Float = self.valueArray[index] as! Float
+        let convertedAmt = amount * unit
+        return "\(convertedAmt)"
+    }
+    
+    //MARK: Keyboard move Up/Down Methods
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.txtAmount.resignFirstResponder()
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
         }
     }
     
